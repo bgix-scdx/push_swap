8b6d1e6da1b23ba7c94e0230d730891e777bef45
bgix-scdx <bastien.gix.etude@gmail.com>
2026-01-13 09:20:43 +0100

    insert ameliorer legerement

--- insert.c
+++ insert.c
@@ -3,31 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   insert.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vgerthof <vgerthof@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bgix <bgix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 12:51:38 by bgix              #+#    #+#             */
-/*   Updated: 2026/01/10 14:07:04 by vgerthof         ###   ########.fr       */
+/*   Updated: 2026/01/12 14:46:39 by bgix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/push_swap.h"
 
-
-//j'ai fais 2 3 modifs mais on est encore asser loin de l'objectif de performance donc faudra optimiser ca
 void insert(t_all *all)
 {
 	t_stack *a;
 	t_stack *b;
+	bool	invert;
 	int		i;
-
+	int 	m;
 	a = &all->a;
 	b = &all->b;
+	i = 0;
 	if (a->size < 2)
 		return ;
 	while (a->size != 2)
 	{
+		i = 0;
+
+		while (a->array[i] != b->size + 1)
+			i++;
+		m = (a->top + (a->size/2)) % a->size;
 		while (a->array[a->top] != b->size + 1)
-			ra(all);
+		{
+			if (m < a->top)
+				if (i > m || i < a->top)
+					ra(all);
+				else
+					rra(all);
+			else
+				if (i > m || i < a->top)
+					ra(all);
+				else
+					rra(all);
+		}
 		pb(all);
 	}
 	if (a->array[a->top] > a->array[(a->top + 1) % a->size])
