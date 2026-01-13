bec1db8c19c0a8b8fcfbf2ff974c0a76eb0fcb55
bgix-scdx <bastien.gix.etude@gmail.com>
2026-01-13 10:05:58 +0100

    insert norminette fixed

--- insert.c
+++ insert.c
@@ -6,46 +6,59 @@
 /*   By: bgix <bgix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 12:51:38 by bgix              #+#    #+#             */
-/*   Updated: 2026/01/12 14:46:39 by bgix             ###   ########.fr       */
+/*   Updated: 2026/01/13 09:59:17 by bgix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/push_swap.h"
 
-void insert(t_all *all)
+void	check_rotation(t_all *all, t_stack *a, int m, int i)
 {
-	t_stack *a;
-	t_stack *b;
-	bool	invert;
+	if (m < a->top)
+	{
+		if (i < m || i > a->top)
+			ra(all);
+		else
+			rra(all);
+	}
+	else
+	{
+		if (i > m || i < a->top)
+			rra(all);
+		else
+			ra(all);
+	}
+}
+
+void	insert_ext(t_all *all, t_stack *a, t_stack *b)
+{
+	int	i;
+	int	m;
+
+	while (a->size != 2)
+	{
+		i = 0;
+		while (a->array[i] != b->size + 1)
+			i++;
+		m = (a->top + (a->size / 2)) % a->size;
+		while (a->array[a->top] != b->size + 1)
+			check_rotation(all, a, m, i);
+		pb(all);
+	}
+}
+
+void	insert(t_all *all)
+{
+	t_stack	*a;
+	t_stack	*b;
 	int		i;
-	int 	m;
+
 	a = &all->a;
 	b = &all->b;
 	i = 0;
 	if (a->size < 2)
 		return ;
-	while (a->size != 2)
-	{
-		i = 0;
-
-		while (a->array[i] != b->size + 1)
-			i++;
-		m = (a->top + (a->size/2)) % a->size;
-		while (a->array[a->top] != b->size + 1)
-		{
-			if (m < a->top)
-				if (i > m || i < a->top)
-					ra(all);
-				else
-					rra(all);
-			else
-				if (i > m || i < a->top)
-					ra(all);
-				else
-					rra(all);
-		}
-		pb(all);
-	}
+	insert_ext(all, a, b);
 	if (a->array[a->top] > a->array[(a->top + 1) % a->size])
 		sa(all);
 	i = b->s_max - 1;
